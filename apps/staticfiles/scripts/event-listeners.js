6f9661e072b1b1036eda519b0aa5947bdda7e028
Adrian McPhee <adrianmcphee@macbook-pro.home>
2024-10-05 21:14:03 +0200

    chore: update gitignore

--- apps/staticfiles/scripts/event-listeners.js
+++
@@ -1,8 +0,0 @@
-let fullNameField = document.getElementById("id_0-full_name");
-let preferredNameField = document.getElementById("id_0-preferred_name");
-
-if (fullNameField) {
-    fullNameField.addEventListener("input", event => {
-        preferredNameField.value = fullNameField.value.split(" ")[0];
-    });
-}
--- apps/staticfiles/scripts/forms.js
+++
@@ -1,5 +0,0 @@
-const input = document.querySelector('[autocomplete=one-time-code');
-
-if(input) {
-  input.addEventListener('input', () => input.style.setProperty('--_otp-digit', input.selectionStart));
-}
